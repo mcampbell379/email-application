fb8267fc1de0a5eb7d1911a76dadc0df7e6a66ec
mcampbell379 <campbell.mitchell45@gmail.com>
2023-05-12 13:16:16 -0500

    added department method

--- src/main/java/com/emailapp/Email.java
+++ src/main/java/com/emailapp/Email.java
@@ -1,5 +1,7 @@
 package com.emailapp;
 
+import java.util.Scanner;
+
 import lombok.ToString;
 
 public class Email {
@@ -14,9 +16,29 @@ public class Email {
     public Email(String firstName, String lastName){
         this.firstName = firstName;
         this.lastName = lastName;
-    }
-    // ask for dept
 
+        // call a method asking for dept which returns the department
+        this.department = setDepartment();
+    }
+
+    // ask for dept
+    private String setDepartment(){
+        System.out.println("DEPARTMENT CODES:\n1 for Sales\n2 for Development\n3 for accounting\n0 for none\nEnter Department Code:");
+
+        Scanner input = new Scanner(System.in);
+        int departmentChoice = input.nextInt();
+
+        if(departmentChoice == 1){
+            return "sales";
+        } else if (departmentChoice == 2){
+            return "dev";
+        } else if (departmentChoice == 3){
+            return "acct";
+        } else {
+            return "";
+        }
+        
+    }
     // generate a random password
 
     // set mailbox capacity
@@ -26,6 +48,6 @@ public class Email {
     // displaying the email
     @Override
     public String toString(){
-        return this.firstName + " " + this.lastName;
+        return this.firstName + " " + this.lastName + " " + this.department;
     }
 }
